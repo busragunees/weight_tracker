ee7c56a3aa459ad560f511597648ac0b1b0e6df9
Busra Gunes <busraaguunes@gmail.com>
2022-07-16 00:28:01 +0300

    Initial Commit

---
+++ weight_tracker_app/weight_tracker_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weight_tracker_appApp.swift
+//  weight_tracker_app
+//
+//  Created by Büşra Güneş on 16.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct weight_tracker_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
