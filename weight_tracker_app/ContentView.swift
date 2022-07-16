1cb0637a9c6a245033732a2594cc85eb7fec2fc1
Busra Gunes <busraaguunes@gmail.com>
2022-07-16 14:55:23 +0300

    initial commit

--- weight_tracker_app/ContentView.swift
+++ weight_tracker_app/ContentView.swift
@@ -7,19 +7,65 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
+
+
+struct TitleView:View{
+    var body:some View{
+        Text("Welcome to \nWeight Tracker").font(.system(size: 30).bold()).multilineTextAlignment(.center).padding(.top,100).foregroundColor(.indigo) //neden olusturdugum renk eklenmedi
+    }
+}
+struct TextView:View{
+    var body: some View{
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Follow your weight in a controlled way, notice the change, stay healthy and fit.").font(.body).foregroundColor(.gray)
+        }
+        
+    }
+}
+
+struct GetStartedButton:View{
+    var body: some View{
+        NavigationLink(destination: HomeView()){
+            Text("Get Started")
+                .frame(width: 200 , height: 50, alignment: .center)
+                .background(Color.indigo)
+                .foregroundColor(Color.white)
+                .cornerRadius(8)
         }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView{
+            
+            ZStack(alignment:.top){
+                Color("bgBlueColor")
+                VStack {
+                    
+                    Image("ic_splash_logo")
+                        .aspectRatio(contentMode: .fit)
+                        .resizable()
+                    VStack(){
+                        /*Image("bg_rounded_white")
+                            .overlay(TitleView(),alignment:.top)
+                            .overlay(TextView(),alignment: .center)
+                            .shadow(color: .gray, radius: 1)
+                            .padding(.top, 1.0)
+                            .overlay(GetStartedButton(),alignment:.bottom)
+                            .frame(height:0)*/
+                    }.background(Image("bg_rounded_white").resizable().edgesIgnoringSafeArea(.all).frame(height:250))
+                }
+            }
+            
+        }
+    }
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
